835093cf597f0109328272b35e9d28f767af9d1e
ali afzali <aliafzali.b@gmail.com>
2023-03-24 00:25:21 +0430

    single-blog completed

--- scripts/globals_body_end.js
+++ scripts/globals_body_end.js
@@ -10,15 +10,12 @@ window.onscroll = function () {
 function scrollFunction() {
   let offsetTop = document.body.scrollTop || document.documentElement.scrollTop;
   let headerElement = document.querySelector(".site-header");
-  if (offsetTop > 50) {
-    headerElement.classList.add("sticky-header");
-    /* document.getElementById("nav").style.top = "0px";
-    document.getElementById("nav-icon").style.top = "-400px"; */
-  } else {
-    headerElement.classList.remove("sticky-header");
-
-    /* document.getElementById("nav").style.top = "-200px";
-    document.getElementById("nav-icon").style.top = "1%"; */
+  if (headerElement) {
+    if (offsetTop > 50) {
+      headerElement.classList.add("sticky-header");
+    } else {
+      headerElement.classList.remove("sticky-header");
+    }
   }
 }
 //window events ens
--- scripts/home.js
+++ scripts/home.js
@@ -120,3 +120,43 @@ function roydadOff() {
 }
 
 /* message */
+const handle_home_page_sec_1_button_click = (el) => {
+  let index = el.getAttribute("data-index");
+  handle_home_page_sec_1_go_to_index(index);
+};
+const handle_home_page_sec_1_next_click = () => {
+  let buttons = document.querySelectorAll(
+    ".buttons-container .single-tabs-btn"
+  );
+  let index = 0;
+  buttons.forEach((btn) => {
+    if (btn.classList.contains("active"))
+      index = btn.getAttribute("data-index");
+  });
+  if (Number(index) > 3) {
+    index = 1;
+  } else {
+    index = Number(index) + 1;
+  }
+  handle_home_page_sec_1_go_to_index(index);
+};
+const handle_home_page_sec_1_go_to_index = (
+  index //number is 1 - 4
+) => {
+  console.log("index", index);
+  let tab_btn = document.querySelector(
+    `.buttons-container .single-tabs-btn:nth-child(${index})`
+  );
+  let dot_btn = document.querySelector(
+    `.sec-1-tab-dots .sec-1-tab-single-dot:nth-child(${index})`
+  );
+  let tab_content = document.querySelector(
+    `.sec-1-content-container .single-tab-content[data-index="${index}"]`
+  );
+  $(".sec-1-tab-dots .sec-1-tab-single-dot").removeClass("active");
+  $(".buttons-container .single-tabs-btn").removeClass("active");
+  $(".sec-1-content-container .single-tab-content").removeClass("active");
+  tab_btn.classList.add("active");
+  dot_btn.classList.add("active");
+  tab_content.classList.add("active");
+};
