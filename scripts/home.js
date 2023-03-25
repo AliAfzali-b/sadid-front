8f84f08190971da71a99ba29dc967c18874bdfba
ali afzali <aliafzali.b@gmail.com>
2023-03-25 14:25:04 +0330

    cleaner code

--- scripts/home.js
+++ scripts/home.js
@@ -1,73 +1,11 @@
-function setSlide(input, index) {
-  let item = document.querySelector(`#${input}`);
-  let itemBtn = document.querySelector(`#${index}`);
-  let slides = [...document.querySelector(".slides").children];
-  let slideIndex = [...document.querySelector(".slider-btns").children];
-  slides.forEach((element) => {
-    element.classList.remove("active");
-  });
-  slideIndex.forEach((element) => {
-    element.classList.remove("selected");
-  });
-  item.classList.add("active");
-  itemBtn.classList.add("selected");
-}
-
-/* ....... footer ....... */
-
-function setSlideVideo(input1, input2) {
-  let item1 = document.querySelector(`#${input1}`);
-  let item2 = document.querySelector(`#${input2}`);
-  let slides1 = [...document.querySelector(".video-slider").children];
-  let slides2 = [...document.querySelector(".video-photos-div").children];
-  slides1.forEach((element) => {
-    element.classList.remove("global-active");
-  });
-  slides2.forEach((element) => {
-    element.classList.remove("video-photo-selected");
-  });
-  item1.classList.add("global-active");
-  item2.classList.add("video-photo-selected");
-}
-
-function setSlide(input, index) {
-  let item = document.querySelector(`#${input}`);
-  let itemBtn = document.querySelector(`#${index}`);
-  let slides = [...document.querySelector(".slides").children];
-  let slideIndex = [...document.querySelector(".slider-btns").children];
-  slides.forEach((element) => {
-    element.classList.remove("active");
-  });
-  slideIndex.forEach((element) => {
-    element.classList.remove("selected");
-  });
-  item.classList.add("active");
-  itemBtn.classList.add("selected");
-}
-
-const msg = document.querySelector(".message");
-function Message() {
-  msg.classList.add;
-}
-
-/* message */
+/* message start*/
 function messageOn() {
   document.getElementById("message-btn-on").style.display = "flex";
 }
 function messageOff() {
   document.getElementById("message-btn-on").style.display = "none";
 }
-
-/* message */
-/* message */
-function roydadOn() {
-  document.getElementById("roydad-div-on").style.display = "flex";
-}
-function roydadOff() {
-  document.getElementById("roydad-div-on").style.display = "none";
-}
-
-/* message */
+/* message end*/
 const handle_home_page_sec_1_button_click = (el) => {
   let index = el.getAttribute("data-index");
   handle_home_page_sec_1_go_to_index(index);
